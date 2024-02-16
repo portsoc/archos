335ca33a0f8be2230e3b470b845f27c940082735
softwarecomplexity <softwarecomplexity@gmail.com>
2024-02-16 05:34:08 +0000

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import 'tailwindcss/tailwind.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './pages/_app';
+import "tailwindcss/tailwind.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./pages/_app";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root"),
 );
